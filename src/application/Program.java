a4c78c431ff32f1fe67a98cddf71223143d268c6
Rafael Ribeiro <rafael.ribeirowt@gmail.com>
2022-10-23 18:56:00 -0300

    Seller Entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,8 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		Seller obj2 = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		System.out.println(obj2);
 	}
 
 }
